b1e98032ae29db3df37fd0fe67883b3ec3381b8a
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2025-07-17 23:54:08 -0300

    update pokeApi

---
+++ assets/js/api/pokeApi.js
@@ -0,0 +1,38 @@
+import { Pokemon } from '../models/pokemon.js';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+/**
+ * Converts detailed API response into a Pokemon instance
+ */
+function transformToPokemon(data) {
+  const types = data.types.map(slot => slot.type.name);
+
+  return new Pokemon({
+    id: data.id,
+    name: data.name,
+    types,
+    image: data.sprites.other['dream_world'].front_default
+  });
+}
+
+/**
+ * Fetches detailed info for a single Pokémon by its URL
+ */
+async function fetchPokemonDetail(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return transformToPokemon(data);
+}
+
+/**
+ * Fetches a page of Pokémon, then fetches details for each one
+ */
+export async function fetchPokemons(offset = 0, limit = 10) {
+  const response = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
+  const data = await response.json();
+
+  const detailPromises = data.results.map(pokemon => fetchPokemonDetail(pokemon.url));
+  const pokemons = await Promise.all(detailPromises);
+  return pokemons;
+}
