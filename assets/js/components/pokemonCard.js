af5d392f02b089413f5ddbfe5f0e0d68caa7b9ad
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2025-07-21 17:03:59 -0300

    update pokemonCard component

--- assets/js/components/pokemonCard.js
+++ assets/js/components/pokemonCard.js
@@ -1,18 +1,22 @@
 export function createPokemonCard(pokemon) {
   return `
-    <li class="pokemon ${pokemon.primaryType}">
-      <span class="number">${pokemon.formattedNumber}</span>
+    <li class="pokemon-card" ${pokemon.primaryType}">
+     
+    <div class="info-container" ><div>
       <span class="name">${pokemon.name}</span>
+       <span class="number">${pokemon.formattedNumber}</span>
 
-      <div class="detail">
+
+
+      <div class="type-container">
         <ol class="types">
           ${pokemon.types
             .map(type => `<li class="type ${type}">${type}</li>`)
             .join('')}
         </ol>
-
-        <img src="${pokemon.image}" alt="${pokemon.name}" />
       </div>
+        <img src="${pokemon.image}" alt="${pokemon.name}" />
+     
     </li>
   `;
 }
