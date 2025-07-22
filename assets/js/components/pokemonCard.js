93b3189fb58f8e053bd43dabdd16b590d42db4cb
Dio Lemos <67412764+Diolemos@users.noreply.github.com>
2025-07-21 21:46:30 -0300

    update pokemonCard Add the back of the card

--- assets/js/components/pokemonCard.js
+++ assets/js/components/pokemonCard.js
@@ -7,20 +7,45 @@ export function createPokemonCard(pokemon) {
         border-color: var(--${pokemon.primaryType}-dark);
       "
     >
-      <div class="info-container">
-        <span class="name">${pokemon.name}</span>
-        <span class="number">${pokemon.formattedNumber}</span>
+      <div class="card-inner">
+        <!-- Front -->
+        <div class="card-front">
+          <div class="info-container">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">${pokemon.formattedNumber}</span>
+          </div>
+          <div class="type-container">
+            <ol class="types">
+              ${pokemon.types
+                .map(
+                  type => `
+                    <li 
+                      class="type ${type}" 
+                      style="
+                        background-color: var(--${type}-main);
+                        color: var(--${type}-darker)
+                      "
+                    >
+                      ${type}
+                    </li>`
+                )
+                .join('')}
+            </ol>
+          </div>
+          <img src="${pokemon.image}" alt="${pokemon.name}" />
+        </div>
+
+        <!-- Back -->
+        <div class="card-back">
+          <h3>Stats</h3>
+          <ul class="stats">
+            <li>HP: ${pokemon.stats.hp}</li>
+            <li>Attack: ${pokemon.stats.attack}</li>
+            <li>Defense: ${pokemon.stats.defense}</li>
+            <li>Speed: ${pokemon.stats.speed}</li>
+          </ul>
+        </div>
       </div>
-      <div class="type-container">
-        <ol class="types">
-          ${pokemon.types
-            .map(
-              type => `<li class="type ${type}" style="background-color: var(--${type}-main);color: var(--${type}-darker)">${type}</li>`
-            )
-            .join('')}
-        </ol>
-      </div>
-      <img src="${pokemon.image}" alt="${pokemon.name}" />
     </li>
   `;
 }
